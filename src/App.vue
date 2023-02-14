5762003f3e15678deca410f65b36d6594e44099c
arzzzzi <kantyshevmagomed@gmail.com>
2023-02-14 17:47:57 +0300

    started learning

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form
+        @create="createPost"
+    />
+    <post-list 
+        :posts="posts"
+    />
+  </div>
 </template>
 
+<script>
+import PostForm from '@/components/PostForm'
+import PostList from '@/components/PostList'
+export default {
+    components: {
+        PostForm, PostList
+    },
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'Javascript', body: 'Описание поста'},
+                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+                {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+            ],
+        }
+    },
+    methods: {
+        createPost (event) {
+            this.posts.push(event)
+        }
+    }   
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .app{
+        padding: 20px;
+    }
+    
+    
 </style>
