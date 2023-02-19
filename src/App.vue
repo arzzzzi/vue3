fa316f22297474fcfe536efcd731a59c73c62d43
arzzzzi <kantyshevmagomed@gmail.com>
2023-02-19 19:59:49 +0300

    pagination added (showing more posts with scroll)

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+    <my-input 
+        v-model="searchQuery"
+        placeholder="Поиск..."
+    />
     <div class="app__btns">
         <my-button
         @click="showDialog"
@@ -20,11 +24,23 @@
     </my-dialog>
     
     <post-list 
-        :posts="sortedPosts"
+        :posts="sortedAndSearchedPosts"
         @remove="removePost"
         v-if="!isLoading"
     />
     <div v-else>Идет загрузка...</div>
+    <div ref="observer"></div>
+    <!-- <div class="page__wrapper">
+        <div
+            v-for="pageNumber in totalPages" 
+            :key="pageNumber"
+            class="page"
+            :class="{
+                'current__page': page === pageNumber
+            }"
+            @click="changePage(pageNumber)"
+        >{{ pageNumber }}</div>
+    </div> -->
   </div>
 </template>
 
@@ -42,6 +58,10 @@ export default {
             dialogVisible: false,
             isLoading: false,
             selectedSort: '',
+            searchQuery: '',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions:[ 
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По содержимому'}
@@ -59,25 +79,64 @@ export default {
         showDialog() {
             this.dialogVisible = true;
         },
+        // changePage(pageNumber) {
+        //     this.page = pageNumber
+        // },
         async fetchPosts() {
             try {
                 this.isLoading = true
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params: {
+                        _page: this.page,
+                        _limit: this.limit
+                    }
+                })
+                this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
                 this.posts = response.data
-                this.isLoading = false
             } catch (error) {
                 alert('Ошибка')
             } finally {
-
+                this.isLoading = false
+            }
+        },
+        async loadMorePosts() {
+            try {
+                this.page += 1
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params: {
+                        _page: this.page,
+                        _limit: this.limit
+                    }
+                })
+                this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+                this.posts = [...this.posts, ...response.data]
+            } catch (error) {
+                alert('Ошибка')
+            } finally {
+                this.isLoading = false
             }
         }
     },
     mounted() {
-        this.fetchPosts()
+        this.fetchPosts();
+        const options = {
+            rootMargin: '0px',
+            threshold: 1.0
+        }
+        const callback = (entries, observer) => {
+            if(entries[0].isIntersecting && this.page < this.totalPages) {
+                this.loadMorePosts()
+            }
+        };
+        const observer = new IntersectionObserver(callback, options);
+        observer.observe(this.$refs.observer)
     },
     computed: {
         sortedPosts() {
             return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+        },
+        sortedAndSearchedPosts() {
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
         }
     },
     watch: {
@@ -85,6 +144,9 @@ export default {
         //     this.posts.sort((post1, post2) => {
         //         return post1[newValue]?.localeCompare(post2[newValue])
         //     })
+        // }
+        // page() {
+        //     this.fetchPosts()
         // }
     }
 }
@@ -104,5 +166,16 @@ export default {
         justify-content: space-between;
         margin: 15px 0;
     }
-    
+    .page__wrapper {
+        display: flex;
+        margin-top: 15px;
+    }
+    .page {
+        border: 1px solid black;
+        padding: 10px;
+    }
+    .current__page {
+       border: 2px solid green; 
+    }
+
 </style>
