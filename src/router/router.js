1d0e74f44f4bd936e26325a658e5b9a0c82bbdb0
arzzzzi <kantyshevmagomed@gmail.com>
2023-02-20 14:05:49 +0300

    dynamic routing

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import Main from '@/pages/Main'
 import PostPage from '@/pages/PostPage'
 import About from '@/pages/About'
+import SinglePostPage from '@/pages/SinglePostPage'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [{
@@ -14,6 +15,10 @@ const routes = [{
     {
         path: '/about',
         component: About
+    },
+    {
+        path: '/posts/:id',
+        component: SinglePostPage
     }
 ]
 
